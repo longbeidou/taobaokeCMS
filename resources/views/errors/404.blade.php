d40b8e25a96299dfc685fd29718e6586b61d84d5
wanglongshuai <1482585838@qq.com>
2018-01-25 20:21:35 +0800

    增加404 505页面

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>404 页面</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link href="/adminstyle/css/bootstrap.min.css?v=3.3.5" rel="stylesheet">
+    <link href="/adminstyle/css/font-awesome.min.css?v=4.4.0" rel="stylesheet">
+    <link href="/adminstyle/css/animate.min.css" rel="stylesheet">
+    <link href="/adminstyle/css/style.min.css?v=4.0.0" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+    <div class="middle-box text-center animated fadeInDown">
+        <h1>404</h1>
+        <h3 class="font-bold">页面未找到！</h3>
+
+        <div class="error-desc">
+            抱歉，页面好像去火星了~
+            <br><br>
+            <a target="_self" href="/" class="btn btn-primary">前往首页瞅瞅~~</a>
+        </div>
+    </div>
+    <script src="/adminstyle/js/jquery.min.js?v=2.1.4"></script>
+    <script src="/adminstyle/js/bootstrap.min.js?v=3.3.5"></script>
+    <script type="text/javascript" src="http://tajs.qq.com/stats?sId=9051096" charset="UTF-8"></script>
+</body>
+
+</html>
---
+++ resources/views/errors/505.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>500错误</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link href="/adminstyle/css/bootstrap.min.css?v=3.3.5" rel="stylesheet">
+    <link href="/adminstyle/css/font-awesome.min.css?v=4.4.0" rel="stylesheet">
+
+    <link href="/adminstyle/css/animate.min.css" rel="stylesheet">
+    <link href="/adminstyle/css/style.min.css?v=4.0.0" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+    <div class="middle-box text-center animated fadeInDown">
+        <h1>500</h1>
+        <h3 class="font-bold">服务器内部错误</h3>
+
+        <div class="error-desc">
+            服务器好像出错了...
+            <br/>您可以返回主页看看
+            <br/><a href="index.html" class="btn btn-primary m-t">主页</a>
+        </div>
+    </div>
+    <script src="/adminstyle/js/jquery.min.js?v=2.1.4"></script>
+    <script src="/adminstyle/js/bootstrap.min.js?v=3.3.5"></script>
+    <script type="text/javascript" src="http://tajs.qq.com/stats?sId=9051096" charset="UTF-8"></script>
+</body>
+
+</html>
