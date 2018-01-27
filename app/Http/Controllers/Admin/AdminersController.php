a1e2a88cf8a0a5276b8b3aa2a18ed3113c55cb69
wanglongshuai <1482585838@qq.com>
2018-01-27 15:03:08 +0800

    修改 编辑管理员个人资料 的逻辑

--- app/Http/Controllers/Admin/AdminersController.php
+++ app/Http/Controllers/Admin/AdminersController.php
@@ -26,9 +26,22 @@ class AdminersController extends Controller
 
     // 编辑用户的个人资料
     public function update (AdminerInfomationRequest $request) {
-      Adminer::where('id', $request->id)->update(['name'=>$request->name, 'email'=>$request->email]);
+      $adminer = Adminer::where('id', $request->id)->first();
+      if ( $adminer->name == $request->name && $adminer->email == $request->email ) {
 
-      return redirect()->route('adminers.edit', $request->id)->with('success', '更新管理员资料成功！');
+        return redirect()->route('adminers.edit', $request->id)->withErrors('提交的信息未修改，请修改后再提交！');
+      }
+      if ($this->isOnlyEmali($request->is, $request->email)) {
+        Adminer::where('id', $request->id)->update(['name'=>$request->name, 'email'=>$request->email]);
+
+        return redirect()->route('adminers.edit', $request->id)->with('success', '更新管理员资料成功！');
+      } else {
+        Adminer::where('id', $request->id)->update(['name'=>$request->name]);
+
+        return redirect()->route('adminers.edit', $request->id)
+                          ->with('success', '更新管理员名称成功！')
+                          ->withErrors('邮箱未更新，原因：没有修改邮箱或提交的邮箱已经被其他用户使用！');
+      }
     }
 
     // 显示修改用户密码的页面
@@ -49,6 +62,26 @@ class AdminersController extends Controller
       } else {
 
         return redirect()->route('adminers.update.password', $request->id)->with('danger', '请输入正确的管理员密码！');
+      }
+    }
+
+    // 判断对应id的管理员的邮箱是否唯一
+    public function isOnlyEmali($id, $email) {
+      if (Adminer::where('email', $email)->count() == 0) {
+
+        return true;
+      } elseif(Adminer::where('email', $email)->count() == 1) {
+        $email_id = Adminer::where('email', $email)->first()->id;
+        if ($email_id == $id) {
+
+          return true;
+        } else {
+
+          return false;
+        }
+      } else {
+
+        return false;
       }
     }
 }
