7aa3870075af806173d0dd8d1030801f5375b771
wanglongshuai <1482585838@qq.com>
2018-03-31 14:48:38 +0800

    增强超级搜索的站内搜索和超级搜索的显示效果

--- resources/views/home/pc/superSearch/result_taobao.blade.php
+++ resources/views/home/pc/superSearch/result_taobao.blade.php
@@ -17,7 +17,7 @@
         <!-- 显示搜索的总结果数量 -->
         <div class="col-sm-12" style="padding: 15px 5px 15px 0px; margin-left: 15px;">
           <div class="" style="background-color: #fff; border-left: 3px solid #ed2a7a; padding: 1px 12px;">
-            <h6 style="color: #333;">在 <strong>淘宝服务器</strong> 中为您找到相关结果约 <strong>{{ $paginaton['count'] }}</strong> 个。 如果对结果不满意，可以尝试进行 <strong title="搜索本网站服务器中收集的淘宝优惠券信息" target="_blank" style="color: #ed2a7a;">站内搜索</strong>。</h6>
+            <h6 style="color: #333;"><strong style="font-size: 14px;  color: #ed2a7a;">超级搜索的结果</strong> &nbsp;&nbsp;&nbsp;&nbsp;在 <strong>淘宝服务器</strong> 中为您找到相关结果约 <strong>{{ $paginaton['count'] }}</strong> 个。 如果对结果不满意，可以尝试进行 <strong title="搜索本网站服务器中收集的淘宝优惠券信息" target="_blank" style="color: #ed2a7a;">站内搜索</strong>。</h6>
           </div>
         </div>
         @inject('couponInfoPre', 'App\Presenters\CouponPresenter')
