42e7098c28fad89f2f9e319c41b33a9682ad4646
wanglongshuai <1482585838@qq.com>
2018-03-31 17:32:42 +0800

    将fonticon改成本地的

--- config/website.php
+++ config/website.php
@@ -15,7 +15,8 @@ return [
 	'domain_beian' => '蜀ICP备15010745号-18',
 
 	// 网站专用的fonticon地址
-	'font_icon_src' => '//at.alicdn.com/t/font_581943_b2zuivwy5g8bmx6r.css',
+	// 'font_icon_src' => '//at.alicdn.com/t/font_581943_b2zuivwy5g8bmx6r.css',
+	'font_icon_src' => '/css/selfIcon/iconfont.css',
 
 	// 特殊页面的展示环境，例如，wechat环境展示淘口令，系统做特殊处理，环境有四种，分别是：wechat,qq,wx,pc
 	'show_from' => ['wechat', 'qq'],
--- resources/views/home/pc/layouts/index.blade.php
+++ resources/views/home/pc/layouts/index.blade.php
@@ -12,7 +12,7 @@
     <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/main.css">
-    <link rel="stylesheet" href="http://at.alicdn.com/t/font_581943_x4tgw84u3naeb3xr.css">
+    <link rel="stylesheet" href="{{ config('website.font_icon_src') }}">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
