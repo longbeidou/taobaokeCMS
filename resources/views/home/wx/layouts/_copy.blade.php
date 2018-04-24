0cd89578f05bfd40542e8adc1187a95dc370b912
wanglongshuai <1482585838@qq.com>
2018-04-24 16:07:35 +0800

    修改移动端底部版权为配置文件的调用

--- resources/views/home/wx/layouts/_copy.blade.php
+++ resources/views/home/wx/layouts/_copy.blade.php
@@ -1,5 +1,5 @@
 <div class="mui-row" style="margin-top: 5px;">
   <div class="mui-col-xs-12 mui-text-center">
-    <p>Copyright©2017-2018 <br>版权归四川龙琴科技有限公司所有！</p>
+    <p>Copyright©2017-2018 <br>版权归{{ config('website.company_name') }}所有！</p>
   </div>
 </div>
