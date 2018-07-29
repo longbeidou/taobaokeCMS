2e61fb857a8ed6745f1d58458274c4feb5fe7b7c
longbeidou <1482585838@qq.com>
2018-07-29 18:17:05 +0800

    Update alimama.php

--- config/alimama.php
+++ config/alimama.php
@@ -3,10 +3,10 @@
 return [
 
 	// 淘宝客api的appkey的值
-	'appkey' => '24567452',
+	'appkey' => '',
 
 	// 淘宝客api的secretkey的值
-	'secretKey' => '59023132261bb01eec7555738091eca5',
+	'secretKey' => '',
 
 	// 淘宝账户的id
 	'id' => '24234234234',
